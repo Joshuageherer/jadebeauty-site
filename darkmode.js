bf1e9d77551bb8f85bf9baa3b423508fdb658339
Joshuageherer <gehrerjoshi@gmail.com>
2025-05-16 11:56:53 +0200

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("dark-toggle").addEventListener("click", () => {
     document.documentElement.toggleAttribute("data-theme", "dark");
